c293e06bbc3504638e854ff455ec4cd9b1ecd60e
Christine Yueh <christinecyueh@gmail.com>
2019-01-17 23:29:57 -0500

    switched to one json file per entry

--- src/pages/entry.js
+++ src/pages/entry.js
@@ -5,19 +5,21 @@ import SEO from '../components/seo'
 import StructTable from '../components/structure'
 import Summary from '../components/summary'
 
-const sum = require('../data/summary.json');
-const data = require('../data/test.json');
+const entry = 'ZWZ_Q9WYE2';
+const entryData = require('../data/' + entry + '.json')
+const sum = entryData['summary'];
+const data = entryData['structures'];
 
 const Entry = () => (
   <Layout>
-    <SEO title='Entry' />
+    <SEO title={entry} />
     <Grid>
       <Row>
-        <h1>{sum[0].fname}_{sum[0].uid}</h1>
+        <h1>{entry}</h1>
         <hr />
         <h3>Summary</h3>
       </Row>
-      <Summary data={sum[0]} />
+      <Summary data={sum} />
       <Row>
         <hr />
         <h3>Structural Details</h3>
