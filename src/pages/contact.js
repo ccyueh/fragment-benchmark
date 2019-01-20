edbfe81579da8b1008e19bd7d0369009e96ccdcf
Christine Yueh <christinecyueh@gmail.com>
2019-01-20 11:23:41 -0500

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ContactPage = () => (
+  <Layout>
+    <h1>Contact</h1>
+    <p>To contact us, please use the contact form <a href="http://structure.bu.edu/contact">here</a>.</p>
+  </Layout>
+)
+
+export default ContactPage
