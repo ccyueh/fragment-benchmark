31a3534a7f37aa853197c57d410fa86050e16fd9
Christine Yueh <christinecyueh@gmail.com>
2019-01-24 14:39:15 -0500

    updated and added carousel of images to index page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const ContactPage = () => (
   <Layout>
+    <SEO title="Contact" />
     <h1>Contact</h1>
     <p>To contact us, please use the contact form <a href="http://structure.bu.edu/contact">here</a>.</p>
   </Layout>
