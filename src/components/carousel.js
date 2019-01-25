6677627f96a72ccb7bd8a38e2700989643a1cb7d
Christine <ccyueh@bu.edu>
2019-01-25 14:53:06 -0500

    added new images to index carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Row, Col, Carousel } from 'react-bootstrap'
 
-const images = ['ZWZ','ZXZ','1SA']
+const images = ['index_1','index_2','index_3','index_4']
 const IndexCarousel = () => (
   <Row>
     <Col xs={12}>
-      <Carousel>
-	{images.map(image => {
-	  return (
-	    <Carousel.Item>
-              <img width={300} height={300} alt=''
-                src={require('../images/' + image + '.png')} />
-              <Carousel.Caption>
-                <h4>{image}</h4>
-              </Carousel.Caption>
-           </Carousel.Item>)}
-	)}
-      </Carousel>
+      <center>
+	<div style={{ display: `flex`, width: `96vmax`, height: `50vmin` }}>
+          <Carousel>
+	    {images.map(image => {
+	      return (
+	        <Carousel.Item>
+                  <img alt='' src={require('../images/' + image + '.png')} />
+                  <Carousel.Caption>
+                    <h4>{image}</h4>
+                  </Carousel.Caption>
+                </Carousel.Item>)}
+	    )}
+          </Carousel>
+	</div>
+      </center>
     </Col>
   </Row>
 )
