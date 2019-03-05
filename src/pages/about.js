c57f525dd3c19c384cf1137e751796669f90ad9b
Christine <ccyueh@bu.edu>
2019-03-05 14:09:39 -0500

    reorganized home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,20 @@
+import { Link } from 'gatsby'
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import IndexCarousel from '../components/carousel'
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About the Fragment Benchmark</h1>
-    <p></p>
+    <Row>
+      <Col xs={12}>
+	<p>As an example, the slideshow below demonstrates how the fragment/ligands for one entry (<Link to='/entry/1LQ_P11142/'><strong>1LQ_P11142</strong></Link>) bind in a consistent orientation to the protein. Use the left and right arrows to navigate between each image, or click on the slideshow to pause it.</p>
+      </Col>
+    </Row>
+    <IndexCarousel />
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import { Link } from 'gatsby'
+import { withPrefix, Link } from 'gatsby'
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import IndexCarousel from '../components/carousel'
 
 const IndexPage = () => (
   <Layout>
@@ -12,10 +11,11 @@ const IndexPage = () => (
     <Row>
       <Col xs={12}>
 	<p>The Fragment Benchmark is a resource for fragment-based drug discovery. This benchmark dataset contains <strong>62</strong> cases of proteins with conserved binding of structurally similar fragments and drug-like ligands. Users can browse entries by protein or fragment data, or download data and visualizations of each set of structures for individual or all entries.</p>
-	<p>As an example, the slideshow below demonstrates how the fragment/ligands for one entry (<Link to='/entry/1LQ_P11142/'><strong>1LQ_P11142</strong></Link>) bind in a consistent orientation to the protein. Use the left and right arrows to navigate between each image, or click on the slideshow to pause it.</p>
       </Col>
     </Row>
-    <IndexCarousel />
+    <center>
+      <img src={withPrefix('/images/index_flow.png')} alt=''/>
+    </center>
     <Row>
       <Col xs={12}>
 	<p>To learn more about the benchmark, visit the <Link to='/about/'>about</Link> page.</p>
