99d6a5deebcf25c824fe86299138f9180e25b7e4
Christine <ccyueh@bu.edu>
2019-03-18 16:43:07 -0400

    added astex set to downloads page

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby'
 import React from 'react'
 import { Button, ButtonGroup } from 'react-bootstrap'
 import Layout from '../components/layout'
@@ -6,7 +5,7 @@ import SEO from '../components/seo'
 
 const fileText = {
   'tables': 'Tables',
-  'structures': 'Structure files',
+  'structures': 'Structures',
 }
 
 const Download = () => (
@@ -19,10 +18,15 @@ const Download = () => (
           return (
             <Button bsStyle='link' download
             href={'../downloads/' + dltype + '.zip'}>
-            {fileText[dltype] + ' (.zip)'}</Button>)
+            {fileText[dltype]}</Button>)
 	  })}
  	</ButtonGroup>
       <p />
+      <p>"Tables" contains JSON files with the structural information displayed on each entry page, and "Structures" contains the PDB structures associated with the fragment/ligand(s) for each entry. When available, <a href='https://ftmap.bu.edu'>FTMap</a> results for two of the unliganded structures--one with the best resolution and one with the best mapping results--are also included in the structures file.</p>
+      <p>Structures are also available for the <a href='https://dx.doi.org/10.1021/acs.jmedchem.7b00366'>Astex set</a>.</p>
+      <Button bsStyle='link' block download href='../downloads/astex.zip'>
+	Structures (Astex set)
+      </Button>
   </Layout>
 )
 
